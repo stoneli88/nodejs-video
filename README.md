2ac7ec8e22943886a24542834374db1c81797640
malik.l <malik.l@tc-gaming.com>
2018-07-20 23:11:16 +0800

    增加少量文档

---
+++ README.md
@@ -0,0 +1,15 @@
+# Nodejs Video player
+
+docker run -it -v D:\Code[HOST]:/video[Container] centos /bin/bash
+
+NGINX(front-end) [PLAYER]
+
+NGINX(back-end) [UPLOADER]
+
+FFMPEG(encode-decode)
+
+MYSQL(data)
+
+REDIS(cache)
+
+MQ(message)
--- ffmpeg/ffmpeg.sh
+++ ffmpeg/ffmpeg.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Thanks to https://trac.ffmpeg.org/wiki/CompilationGuide/Centos
 installCompilingTools() {
     echo -e "\n Installing compiling tools."
     yum  -y install autoconf automake bzip2 cmake freetype-devel gcc gcc-c++ git libtool make mercurial pkgconfig zlib-devel x264-devel
